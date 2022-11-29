194367236ed2baf726ea2422c5afbca95f195ae9
Aleksei Kudriavtcev <ackudryavcev@gmail.com>
2022-11-29 16:14:00 +0100

    adding correct button work and correct count right answers

--- src/questionsRecord.js
+++ src/questionsRecord.js
@@ -1,4 +1,3 @@
-
 // -----Use this method to access up-to-date Question records-----
 
 // -----How to use this in your page-----
@@ -6,14 +5,15 @@
 // then: const randomValue = getQuestionRecord();
 // to reach specific element you should type:  randomValue.currentIndex
 const getQuestionRecord = () => {
-  let questionRecord = JSON.parse(localStorage.getItem('questionRecord'));
-  if(!questionRecord){
-    questionRecord = {
-      currentIndex : 0,
-      totalCorrectAnswers: 0
+    let questionRecord = JSON.parse(localStorage.getItem('questionRecord'));
+    if (!questionRecord) {
+        questionRecord = {
+            currentIndex: 0,
+            totalCorrectAnswers: 0,
+            userAnswers: [],
+        }
     }
-  }
-  return questionRecord;
+    return questionRecord;
 }
 
 //-----Use this method to set question records-----
@@ -28,26 +28,44 @@ const getQuestionRecord = () => {
 // First: import { setQuestionRecord } from '../questionsRecord.js';
 // Then: setQuestionRecord('correct', quizData.currentQuestionIndex);
 
-const setQuestionRecord = (status, currentQuestion) => {
-  let correctAnswers = Object.values(getQuestionRecord())[1];
-  switch (status) {
-    case 'reset':
-      correctAnswers = 0;
-      break;
+const setQuestionRecord = (status, currentQuestion, correctAnswer, userAnswer) => {
+    let correctAnswers = Object.values(getQuestionRecord())[1];
+    const questionRecord = getQuestionRecord();
+    switch (status) {
+        case 'reset':
+            correctAnswers = 0;
+            questionRecord.userAnswers = [];
+            break;
 
-      case 'correct':
-      correctAnswers++;
-      break;
+        case 'correct':
+            correctAnswers++;
+            questionRecord.userAnswers[currentQuestion] = {
+                question: currentQuestion,
+                answerIsCorrect: true,
+                correctAnswer,
+                userAnswer
+            };
+            break;
+        case 'incorrect':
+            questionRecord.userAnswers[currentQuestion] = {
+                question: currentQuestion,
+                answerIsCorrect: false,
+                correctAnswer,
+                userAnswer
+            };
+        default:
+            break;
+    }
 
-    default:
-      break;
-  }
 
-  localStorage.setItem('questionRecord', JSON.stringify({
-      currentIndex: currentQuestion,
-      totalCorrectAnswers: correctAnswers
+    localStorage.setItem('questionRecord', JSON.stringify({
+        currentIndex: currentQuestion,
+        totalCorrectAnswers: correctAnswers,
+        userAnswers: questionRecord.userAnswers
     }))
+
+
 }
 
-export {getQuestionRecord}
-export {setQuestionRecord}
+export { getQuestionRecord }
+export { setQuestionRecord }
--- src/views/recordView.js
+++ src/views/recordView.js
@@ -2,16 +2,20 @@
  * Create a full question element
  * @returns {Element}
  */
+import { quizData } from '../data.js';
+
+
+
 export const createStatusElement = (status) => {
-  //Status has up-to-date question status
-  const element = document.createElement('div');
-  element.classList.add('status');
-  //We put status information into h1 >> into element: which is div, created above
-  element.innerHTML = String.raw`
-    <h2>${status.totalCorrectAnswers} / ${status.currentIndex + 1}</h2> 
+    //Status has up-to-date question status
+    const element = document.createElement('div');
+    element.classList.add('status');
+    //We put status information into h1 >> into element: which is div, created above
+    element.innerHTML = String.raw `
+    <h2>${status.totalCorrectAnswers} / ${quizData.questions.length}</h2> 
     <h1>${status.currentIndex + 1}</h1>
     <h2>time</h2>
   `; // +1 in the end because index start from 0
 
-  return element;
+    return element;
 };
