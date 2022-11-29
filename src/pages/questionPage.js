166048b7bf691cac8599435dd621e90390b6a96d
Aleksei Kudriavtcev <ackudryavcev@gmail.com>
2022-11-29 22:21:51 +0100

    we can check correct answer and highlight it and work button next and prev

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -1,36 +1,62 @@
 import {
-  ANSWERS_LIST_ID,
-  NEXT_QUESTION_BUTTON_ID,
-  USER_INTERFACE_ID,
+    ANSWERS_LIST_ID,
+    NEXT_QUESTION_BUTTON_ID,
+    PREV_QUESTION_BUTTON_ID,
+    USER_INTERFACE_ID,
 } from '../constants.js';
 import { createQuestionElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
 import { quizData } from '../data.js';
-
 export const initQuestionPage = () => {
-  const userInterface = document.getElementById(USER_INTERFACE_ID);
-  userInterface.innerHTML = '';
-
-  const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
-
-  const questionElement = createQuestionElement(currentQuestion.text);
-
-  userInterface.appendChild(questionElement);
-
-  const answersListElement = document.getElementById(ANSWERS_LIST_ID);
-
-  for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
-    const answerElement = createAnswerElement(key, answerText);
-    answersListElement.appendChild(answerElement);
-  }
-
-  document
-    .getElementById(NEXT_QUESTION_BUTTON_ID)
-    .addEventListener('click', nextQuestion);
+    const userInterface = document.getElementById(USER_INTERFACE_ID);
+    userInterface.innerHTML = '';
+    const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+    console.log(currentQuestion.selected);
+    const questionElement = createQuestionElement(currentQuestion.text);
+    userInterface.appendChild(questionElement);
+    const answersListElement = document.getElementById(ANSWERS_LIST_ID);
+    for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
+        const answerElement = createAnswerElement(key, answerText);
+        answersListElement.appendChild(answerElement);
+        if (key === currentQuestion.correct) {
+            answerElement.classList.add('correct-answer');
+        }
+        answerElement.addEventListener('click', (event) => checkAnswer(key, event.target));
+        if (currentQuestion.selected === key) {
+            if (key === currentQuestion.correct) {
+                answerElement.classList.add('correct');
+            } else {
+                answerElement.classList.add('incorrect');
+            }
+        }
+    }
+    document
+        .getElementById(NEXT_QUESTION_BUTTON_ID)
+        .addEventListener('click', nextQuestion);
+    document
+        .getElementById(PREV_QUESTION_BUTTON_ID)
+        .addEventListener('click', prevQuestion);
+    if (currentQuestion.selected) {
+        answersListElement.classList.add('disabled');
+    }
 };
-
 const nextQuestion = () => {
-  quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
-
-  initQuestionPage();
+    quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
+    initQuestionPage();
 };
+const prevQuestion = () => {
+    quizData.currentQuestionIndex = quizData.currentQuestionIndex - 1;
+    initQuestionPage();
+};
+
+function checkAnswer(answerKey, answerElement) {
+    const answersListElement = document.getElementById(ANSWERS_LIST_ID);
+    answersListElement.classList.add('disabled');
+    const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+    if (answerKey === currentQuestion.correct) {
+        answerElement.classList.add('correct');
+    } else {
+        answerElement.classList.add('incorrect');
+    }
+    currentQuestion.selected = answerKey;
+}
--- src/views/questionView.js
+++ src/views/questionView.js
@@ -1,24 +1,32 @@
 import { ANSWERS_LIST_ID } from '../constants.js';
 import { NEXT_QUESTION_BUTTON_ID } from '../constants.js';
+import { PREV_QUESTION_BUTTON_ID } from '../constants.js';
+import { DONT_KNOW_QUESTION_BUTTON_ID } from '../constants.js';
 
 /**
  * Create a full question element
  * @returns {Element}
  */
 export const createQuestionElement = (question) => {
-  const element = document.createElement('div');
+    const element = document.createElement('div');
 
-  // I use String.raw just to get fancy colors for the HTML in VS Code.
-  element.innerHTML = String.raw`
-    <h1>${question}</h1>
+    // I use String.raw just to get fancy colors for the HTML in VS Code.
+    element.innerHTML = String.raw `
+  <h1>${question}</h1>
+  <ul id="${ANSWERS_LIST_ID}">
+  </ul>
+  <div class ="buttonContainer">
+  <button id="${PREV_QUESTION_BUTTON_ID}">
+    Previous question
+  </button>
+  <button id="${DONT_KNOW_QUESTION_BUTTON_ID}">
+    I don't know
+  </button>
+  <button id="${NEXT_QUESTION_BUTTON_ID}">
+    Next question
+  </button>
+  </div>
+`;
 
-    <ul id="${ANSWERS_LIST_ID}">
-    </ul>
-
-    <button id="${NEXT_QUESTION_BUTTON_ID}">
-      Next question
-    </button>
-  `;
-
-  return element;
+    return element;
 };
