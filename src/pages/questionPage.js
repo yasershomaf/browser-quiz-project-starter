465e1286b617d7b06d382c35251744cd7fada403
Aleksei Kudriavtcev <ackudryavcev@gmail.com>
2022-11-30 09:06:47 +0100

    added correct work burron i dont know and i hid button prev question on the first question and chenge button next question to I finished on the last question

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -2,6 +2,7 @@ import {
     ANSWERS_LIST_ID,
     NEXT_QUESTION_BUTTON_ID,
     PREV_QUESTION_BUTTON_ID,
+    DONT_KNOW_QUESTION_BUTTON_ID,
     USER_INTERFACE_ID,
 } from '../constants.js';
 import { createQuestionElement } from '../views/questionView.js';
@@ -11,7 +12,6 @@ export const initQuestionPage = () => {
     const userInterface = document.getElementById(USER_INTERFACE_ID);
     userInterface.innerHTML = '';
     const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
-    console.log(currentQuestion.selected);
     const questionElement = createQuestionElement(currentQuestion.text);
     userInterface.appendChild(questionElement);
     const answersListElement = document.getElementById(ANSWERS_LIST_ID);
@@ -30,9 +30,18 @@ export const initQuestionPage = () => {
             }
         }
     }
+    if (quizData.currentQuestionIndex === 0) {
+        document.getElementById(PREV_QUESTION_BUTTON_ID).style.display = 'none';
+    }
+    if (quizData.currentQuestionIndex === quizData.questions.length - 1) {
+        document.getElementById(NEXT_QUESTION_BUTTON_ID).textContent = 'I finished';
+    }
     document
         .getElementById(NEXT_QUESTION_BUTTON_ID)
         .addEventListener('click', nextQuestion);
+    document
+        .getElementById(DONT_KNOW_QUESTION_BUTTON_ID)
+        .addEventListener('click', () => showAnswer(key));
     document
         .getElementById(PREV_QUESTION_BUTTON_ID)
         .addEventListener('click', prevQuestion);
@@ -58,5 +67,12 @@ function checkAnswer(answerKey, answerElement) {
     } else {
         answerElement.classList.add('incorrect');
     }
+    currentQuestion.selected = answerKey;
+}
+
+function showAnswer(answerKey) {
+    const answersListElement = document.getElementById(ANSWERS_LIST_ID);
+    answersListElement.classList.add('disabled');
+    const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
     currentQuestion.selected = answerKey;
 }
