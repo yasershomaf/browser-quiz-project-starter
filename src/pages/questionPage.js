7131c625f42c0cbd08d2c61ee01bc4f8aeb7b55a
Huzthepro <hzyfakcay@gmail.com>
2022-12-03 03:07:36 +0100

    First merge attempt

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -7,17 +7,49 @@ import {
   USER_INTERFACE_ID,
   DO_NOT_KNOW_KEY,
   STORED_DATA,
+  COUNTER_ELEMENT,
+  START_TIME,
+  USEFUL_LINKS_ID,
 } from '../constants.js';
 import { createQuestionElement } from '../views/questionView.js';
 import { createAnswerElement } from '../views/answerView.js';
-import { createButtonElement } from '../views/buttonView.js';
 import { quizData } from '../data.js';
-// import { initResultsPage } from './resultsPage.js';
-export const initQuestionPage = () => {
+import { createCounterElement } from '../views/counterView.js';
+import { createQuestionNumberElement } from '../views/questionNumberView.js';
+import { createTimerElement } from '../views/timerView.js';
+import { createUsefulLinkElement } from '../views/usefulLink.js';
+import { initResultsPage } from './resultsPage.js';
+
+let startTime;
+let timer;
+
+export const initQuestionPage = (resetTime) => {
+  if (!startTime) {
+    startTime = JSON.parse(localStorage.getItem(START_TIME));
+
+    if (!startTime) {
+      startTime = 0;
+    }
+  }
+  if (resetTime) {
+    startTime = 0;
+  }
   const userInterface = document.getElementById(USER_INTERFACE_ID);
   userInterface.innerHTML = '';
+  const counterElement = createCounterElement();
+  userInterface.appendChild(counterElement);
+  const questionNumberElement = createQuestionNumberElement();
+  userInterface.appendChild(questionNumberElement);
+  const timerElement = createTimerElement();
+  userInterface.appendChild(timerElement);
+  clearTimeout(timer);
+  // if(!timer){
+  timer = setTimeout(() => {
+    updateTime(timerElement);
+  }, 1000);
+  // }
+
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
-  console.log(currentQuestion.selected);
   const questionElement = createQuestionElement(currentQuestion.text);
   userInterface.appendChild(questionElement);
   const answersListElement = document.getElementById(ANSWERS_LIST_ID);
@@ -38,8 +70,19 @@ export const initQuestionPage = () => {
       }
     }
   }
+
+  const usefulLinksElement = document.getElementById(USEFUL_LINKS_ID);
+  usefulLinksElement.classList.add('hidden');
+  currentQuestion.links.forEach((link) => {
+    const linkElement = createUsefulLinkElement(link);
+    usefulLinksElement.appendChild(linkElement);
+  });
+
   const resultsButton = document.getElementById(RESULTS_BUTTON_ID);
-  // resultsButton.addEventListener('click', initResultsPage );
+  resultsButton.addEventListener('click', () => {
+    clearTimeout(timer);
+    initResultsPage(formatTime(startTime));
+  });
   resultsButton.classList.add('hidden');
 
   const nextButton = document.getElementById(NEXT_QUESTION_BUTTON_ID);
@@ -59,10 +102,9 @@ export const initQuestionPage = () => {
   if (currentQuestion.selected) {
     doNotKnowButton.classList.add('hidden');
     answersListElement.classList.add('disabled');
+    usefulLinksElement.classList.remove('hidden');
   }
 };
-
-//------------- BUTTON FUNCTIONS -------------------
 const nextQuestion = () => {
   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
   initQuestionPage();
@@ -78,6 +120,9 @@ const setAnswer = () => {
   const doNotKnowButton = document.getElementById(DONT_KNOW_QUESTION_BUTTON_ID);
   doNotKnowButton.classList.add('hidden');
 
+  const usefulLinksElement = document.getElementById(USEFUL_LINKS_ID);
+  usefulLinksElement.classList.remove('hidden');
+
   localStorage.setItem(
     STORED_DATA,
     JSON.stringify(quizData.questions.map((question) => question.selected))
@@ -87,13 +132,50 @@ const setAnswer = () => {
 function checkAnswer(answerKey, answerElement) {
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
   if (answerKey === currentQuestion.correct) {
+    const amountOfCorrectAnswers = quizData.questions.filter(
+      (question) => question.selected === question.correct
+    ).length;
+    document.getElementById(COUNTER_ELEMENT).textContent = `${
+      amountOfCorrectAnswers + 1
+    } / ${quizData.questions.length}`;
+
     answerElement.classList.add('correct');
   } else {
     answerElement.classList.add('incorrect');
   }
   currentQuestion.selected = answerKey;
+
+  setAnswer();
 }
 const showAnswer = () => {
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
   currentQuestion.selected = DO_NOT_KNOW_KEY;
+  setAnswer();
+};
+const formatTime = (time) => {
+  const minutes = Math.floor(time / 60); // 60 (seconds per minute)
+  const seconds = time - minutes * 60;
+  return `${('0' + minutes).slice(-2)} : ${('0' + seconds).slice(-2)}`;
+};
+
+const updateTime = (timerElement) => {
+  startTime++;
+
+  localStorage.setItem(START_TIME, JSON.stringify(startTime));
+
+  const formattedTime = formatTime(startTime);
+
+  if (startTime === 600) {
+    // 600 = 10 (minutes) * 60 (seconds per minute)
+    clearTimeout(timer);
+    initResultsPage(formattedTime);
+
+    return;
+  }
+
+  timerElement.textContent = `Time: ${formattedTime}`;
+
+  timer = setTimeout(() => {
+    updateTime(timerElement);
+  }, 1000);
 };
